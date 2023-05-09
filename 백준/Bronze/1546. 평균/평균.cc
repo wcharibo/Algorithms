62425cd4c8e4649c4a56c1ba98dd49c80359ae5a
wcharibo <80238487+wcharibo@users.noreply.github.com>
2023-05-09 23:10:07 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main(int argc, const char *argv[]) {
+ 
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+ 
+	int N;	// 시험 본 과목 개수
+	double sum = 0;
+	cin >> N;
+ 
+	double arr[1001];
+	for(int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+ 
+	sort(arr, arr + N);		// 배열 오름차순 정렬
+	
+	// 가장 큰 점수는 가장 마지막 인덱스에 위치하게 됨
+	for(int i = 0; i < N; i++) {
+		sum = sum + (arr[i] / arr[N - 1]) * 100;	// (조작 된 성적 = (과목 / 최고 성적) * 100) 의 누적 합
+	}
+ 
+	cout << sum / N;	// 성적 평균
+	return 0;
+}
