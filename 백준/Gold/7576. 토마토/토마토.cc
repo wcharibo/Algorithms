af2b7af6416dadd678d4f74ac12e1c1d5fd6919f
wcharibo <80238487+wcharibo@users.noreply.github.com>
2023-07-26 02:18:47 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 10256 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -7,40 +7,40 @@ using namespace std;
 vector <vector<int> > tmtMat;
 int tmtArr, tmtCol, result;
 void ripe();
-queue <pair <int, pair <int, int> > > q;
-pair <int, pair <int, int> > temp;
+pair <int, int>  temp;
+queue <pair <int, int> > q;
 
 int main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     
-    vector <pair <int,int> > ripeTmt;
     cin>>tmtCol>>tmtArr;
     tmtMat.resize(tmtArr, vector <int>(tmtCol, 0));
     for(int i = 0; i< tmtArr;i++){
         for(int j = 0; j<tmtCol;j++){
             cin>>tmtMat[i][j];
-            if(tmtMat[i][j]==1) q.emplace(make_pair(0, make_pair(i,j)));
+            if(tmtMat[i][j]==1) q.emplace(make_pair(i,j));
         }
     }
     int flag=1;
-    for(int i=0; i<tmtArr; i++){
-            for(int j=0; j<tmtCol; j++){
-                if(tmtMat[i][j]==0) {
-                    flag=0;
-                    break;
-                }
+    for(int i = 0; i< tmtArr;i++){
+        for(int j = 0; j<tmtCol;j++){
+            if(tmtMat[i][j]==0){
+                flag=0;
+                break;
             }
-            if(flag==0) break;
         }
+        if(flag==0) break;
+    }
     if(flag==1){
         cout<<0;
         return 0;
     }
+    
     if(q.empty()){
         result = -1;
-        cout<<result;
-        return 0 ;
+        cout<< result;
+        return 0;
     }
     
     ripe();
@@ -52,45 +52,45 @@ void ripe(){
     temp = q.front();
     q.pop();
     
-    int a = temp.first+1;
-    if(temp.second.first-1 >= 0 && tmtMat[temp.second.first-1][temp.second.second]==0){
-        q.push(make_pair(a, make_pair(temp.second.first-1,temp.second.second)));
-        tmtMat[temp.second.first-1][temp.second.second]=1;
+    int a = tmtMat[temp.first][temp.second]+1;
+    if(temp.first-1 >= 0 && tmtMat[temp.first-1][temp.second]==0){
+        q.emplace(make_pair(temp.first-1,temp.second));
+        tmtMat[temp.first-1][temp.second]=a;
     }
-    if(temp.second.second-1 >= 0 && tmtMat[temp.second.first][temp.second.second-1]==0){
-        q.push(make_pair(a, make_pair(temp.second.first,temp.second.second-1)));
-        tmtMat[temp.second.first][temp.second.second-1]=1;
+    if(temp.second-1 >= 0 && tmtMat[temp.first][temp.second-1]==0){
+        q.emplace(make_pair(temp.first,temp.second-1));
+        tmtMat[temp.first][temp.second-1]=a;
     }
-    if(temp.second.first+1 < tmtArr && tmtMat[temp.second.first+1][temp.second.second]==0){
-        q.push(make_pair(a, make_pair(temp.second.first+1,temp.second.second)));
-        tmtMat[temp.second.first+1][temp.second.second]=1;
+    if(temp.first+1 < tmtArr && tmtMat[temp.first+1][temp.second]==0){
+        q.emplace(make_pair(temp.first+1,temp.second));
+        tmtMat[temp.first+1][temp.second]=a;
     }
-    if(temp.second.second+1 < tmtCol && tmtMat[temp.second.first][temp.second.second+1]==0){
-        q.push(make_pair(a, make_pair(temp.second.first,temp.second.second+1)));
-        tmtMat[temp.second.first][temp.second.second+1]=1;
+    if(temp.second+1 < tmtCol && tmtMat[temp.first][temp.second+1]==0){
+        q.emplace(make_pair(temp.first,temp.second+1));
+        tmtMat[temp.first][temp.second+1]=a;
     }
     
     if(q.empty()){
-        result = -1;
-        return;
-    }
-    
-    if(temp.first!=q.front().first){
+        
         int flag=1;
         for(int i=0; i<tmtArr; i++){
             for(int j=0; j<tmtCol; j++){
                 if(tmtMat[i][j]==0) {
                     flag=0;
-                    break;
+                    result = -1;
+                    return;
                 }
             }
-            if(flag==0) break;
         }
         
         if(flag==1){
-            result = temp.first+1;
+            result = tmtMat[temp.first][temp.second]-1;
             return;
-        }
+        
+    }
+        
+    
+        
     }
     
     ripe();
