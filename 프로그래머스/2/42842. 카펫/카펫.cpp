f01c4bfe7b1c23ee36cf5cf11bd249c03029e673
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-11 16:18:56 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -6,20 +6,14 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    vector<pair<int, int>> pos_table;
-    
-    for(int i = 1; i<= sqrt(yellow); i++){
-        if(yellow%i==0) pos_table.push_back({yellow/i, i});
-    }
-    
-    for(pair<int, int> a : pos_table){
-        int temp = (a.first+2+a.second)*2;
-        if(temp == brown){
-            answer.push_back(max(a.first+2, a.second+2));
-            answer.push_back(min(a.first+2, a.second+2));
+    int i = 1;
+    while(1){
+        if(yellow%i==0 && (yellow/i+i+2)*2==brown){
+            answer.push_back(max(yellow/i+2, i+2));
+            answer.push_back(min(yellow/i+2, i+2));
             break;
         }
+        i++;
     }
-    
     return answer;
 }
