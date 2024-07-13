096387123fbe3f35e2a470c791144f078a105af7
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-13 17:58:23 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,19 +1,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     vector<int> answer;
-    
+    unordered_map<string, int> word_table;
     int wrong ,flag = 1;
     
+    word_table.insert({words[0], 1});
+    
     for(int i =1; i<words.size(); i++){
-        if(flag!=0 && (*(words[i-1].end()-1) != words[i][0] || find(words.begin(), words.begin()+i, words[i]) != words.begin()+i)){
+        if(flag!=0 && (*(words[i-1].end()-1) != words[i][0] || word_table.find(words[i]) != word_table.end())){
             flag=0;
             wrong = i+1;//번째로 기록하기 위함
         }
+        word_table.insert({words[i], 1});
     }
     
     if(flag==0){
