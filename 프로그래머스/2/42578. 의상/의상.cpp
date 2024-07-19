2fcd3acfd29eb08a4d12866dd5a260ddaf560c7b
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-19 14:00:49 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string, int> clo_ctg;
+    
+    for(vector<string> a : clothes){
+        if(clo_ctg.find(a[1])==clo_ctg.end()){
+            clo_ctg.insert({a[1], 1});
+        }
+        else clo_ctg[a[1]]++;
+    }
+    
+    for(auto i = clo_ctg.begin(); i!=clo_ctg.end(); i++) answer*=(++(i->second));//2벌인 경우 안입기, 1번 입기, 2번 입기 로 3가지 경우로 다뤄주기 위해 1을 더해서 곱해줌
+    answer--;//아무것도 입지 않은 경우
+    return answer;
+}
