9b5d24615c109cdd59f2405b575cc39697179fb8
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-19 13:13:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.09 ms, Memory: 5.66 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -5,21 +5,12 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer(arr1.size());
-    vector<vector<int>> arr_sum(arr1.size()*arr1[0].size());
-    
-    for(int i = 0; i < arr_sum.size(); i++) arr_sum[i].resize(arr2.size()*arr2[0].size());
-    
-    for(int i = 0; i < arr_sum.size(); i++){
-        for(int j = 0; j < arr_sum[i].size(); j++){
-            arr_sum[i][j] = arr1[i/arr1[0].size()][i%arr1[0].size()]*arr2[j/arr2[0].size()][j%arr2[0].size()];
-        }
-    }
     
     for(int i = 0; i < arr1.size(); i++){
         for(int j = 0; j < arr2[0].size(); j++){
             int temp = 0;
             for(int k = 0; k < arr2.size(); k++){
-                temp+=arr_sum[k+arr2.size()*i][k*arr2[0].size()+j];
+                temp+=(arr1[i][k]*arr2[k][j]);
             }
             answer[i].push_back(temp);
         }
