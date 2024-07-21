a7d85f572c9d1a58e106d0413d3c85f02623893a
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-21 20:55:32 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0, flag=0;
+    
+    sort(citations.begin(), citations.end());
+    
+    int cit_size = citations.size();
+    
+    int h = min(citations[0], cit_size);
+    
+    while(flag==0){
+        int temp = 0;
+        for(int i : citations) if(i>=h) temp++;
+        if(temp>=h) h++;
+        else{
+            answer=--h;
+            flag=1;
+        }
+    }
+    
+    return answer;
+}
