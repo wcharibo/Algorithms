9ed6cbc4933261dba658063950e6b22284e3f25e
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-07-25 17:13:13 +0900

    [level 2] Title: 튜플, Time: 17.59 ms, Memory: 6.63 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -8,17 +8,18 @@ using namespace std;
 vector<int> solution(string s) {
     vector<int> answer, temp;
     vector<pair<int,int>> conclusion;
-    string str_nums, str_seg;
+    string str_num, str_seg;
     int max=0;
     
-    for(int i = 0; i< s.size(); i++){
-        if(s[i]!='{' && s[i]!='}') str_nums+=s[i];
-    }
-    
-    istringstream iss(str_nums);
-    while(getline(iss, str_seg, ',')){
-        temp.push_back(stoi(str_seg));
-        if(temp.back()>max) max = temp.back();
+    for(char i : s){
+        if(i-'0'>=0 && i-'0'<=9) str_num+=i;
+        else{
+            if(str_num.size()){
+                temp.push_back(stoi(str_num));
+                if(temp.back()>max) max = temp.back();
+            }
+            str_num.clear();
+        }
     }
     
     conclusion.resize(max+1, {0,0});
