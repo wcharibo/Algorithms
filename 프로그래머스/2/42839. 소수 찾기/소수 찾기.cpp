4b88eaf0a91b759933e2f9035d71b03cfc904ece
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-09-06 01:41:01 +0900

    [level 2] Title: 소수 찾기, Time: 96.82 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,54 +1,44 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+#include <set>
+#include <cstring>
 
 using namespace std;
+int answer = 0;
+bool visited[10];
+set<int> s;
 
-bool cal(string &a, string &b){
-    return a+b > b+a;
+bool check(string str) {
+    int a = stod(str);
+    if (a < 2) {
+        return false;
+    }
+    for (int i = 2; i < a; i++) {
+        if (a%i == 0) {
+            return false;
+        }
+    }
+    s.insert(a);
+    return true;
+}
+
+void dfs(string numbers, string str) {
+    if (str != "") {
+        check(str);
+    }
+    for (int i = 0; i < numbers.length(); i++) {
+        if (!visited[i]) {
+            visited[i] = 1;
+            dfs(numbers, str + numbers[i]);
+            visited[i] = 0;
+        }
+    }
 }
 
 int solution(string numbers) {
-    int max, answer = 0;
-    string max_str = "";
-    vector<string> nums;
-    vector<int> repeat_cnt(10,0);
-    for(int i = 0; i < numbers.size(); i++){
-        string temp{numbers[i]};
-        nums.push_back(temp);
-        repeat_cnt[stoi(temp)]++;
-    }
-    sort(nums.begin(), nums.end(), cal);
-    
-    for(string i : nums) max_str.append(i);
-    
-    max = stoi(max_str);
-    int cnt_max = sqrt(max)+1;
-    
-    vector<int> decimal(max+1, 1);
-    decimal[0] = 0;
-    decimal[1] = 0;
-    
-    for(int i = 2; i <= cnt_max; i++){
-        if(decimal[i] == 0) continue;
-        for(int j = i+1; j < decimal.size(); j++) if(decimal[j] == 1 && j%i==0) decimal[j]=0;
-    }
-    
-    vector<string> str_decimal;
-    for(int i = 0; i < decimal.size(); i++) if(decimal[i]==1) str_decimal.push_back(to_string(i));
-    
-    for(string temp : str_decimal){
-        vector<int> repeat_temp(repeat_cnt);
-        int flag = 0;
-        for(int i = 0; i < temp.size(); i++){
-            repeat_temp[temp[i] - '0']--;
-        }
-        for(int j : repeat_temp) if(j<0) flag=1;
-        
-        if(flag==0) answer++;
-    }
-    
-    
+    memset(visited, false, sizeof(visited));
+    dfs(numbers, "");
+    answer = s.size();
+    s.clear();
     return answer;
 }
