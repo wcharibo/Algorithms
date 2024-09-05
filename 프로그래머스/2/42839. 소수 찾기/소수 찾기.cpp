fda7fc29a8d329cfe1be3a53df0c5ec671edaa10
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-09-06 02:01:40 +0900

    [level 2] Title: 소수 찾기, Time: 2665.74 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,45 +1,54 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
+#include <cmath>
+
 using namespace std;
-map<int, int> check;
 
-int check_prime(vector<int> v){
-    int ret = 0;
-    sort(v.begin(),v.end());
-    do{
-        int j = 1, prime = 0;
-        for(int i=v.size()-1;i>=0;i--){
-            prime += (v[i] * j);
-            j*=10;
-        }
-        if(check[prime] != 0 || prime <2) {continue;}
-        check[prime]++;
-        bool sw = false;
-        for(int i=2;i<prime;i++){
-            if(prime%i == 0) {sw = true; break;}
-        }
-        if(sw == false) ret++;
-    }while(next_permutation(v.begin(),v.end()));
-    return ret;
+bool cal(string &a, string &b){
+    return a+b > b+a;
 }
-int solution(string numbers) {
-    int answer = 0;
-    vector<int> v,mask;
-    for(int i=0;i<numbers.length();i++)
-        v.push_back(numbers[i]-48);
-    mask.assign(v.size(),0);
 
-    for(int i=0;i<v.size();i++){
-        mask[i] = 1;
-        do{
-            vector<int> temp;
-            for(int i=0;i<mask.size();i++){
-                if(mask[i] == 1) temp.push_back(v[i]);
-            }
-            answer += check_prime(temp);
-        }while(prev_permutation(mask.begin(),mask.end()));
+int solution(string numbers) {
+    int max, answer = 0;
+    string max_str = "";
+    vector<string> nums;
+    vector<int> repeat_cnt(10,0);
+    for(int i = 0; i < numbers.size(); i++){
+        string temp{numbers[i]};
+        nums.push_back(temp);
+        repeat_cnt[numbers[i] - '0']++;
     }
+    sort(nums.begin(), nums.end(), cal);
+    
+    for(string i : nums) max_str.append(i);
+    
+    max = stoi(max_str);
+    int cnt_max = sqrt(max)+1;
+    
+    vector<int> decimal(max+1, 1);
+    decimal[0] = 0;
+    decimal[1] = 0;
+    
+    for(int i = 2; i <= cnt_max; i++){
+        if(decimal[i] == 0) continue;
+        for(int j = i+1; j < decimal.size(); j++) if(decimal[j] == 1 && j%i==0) decimal[j]=0;
+    }
+    
+    vector<string> str_decimal;
+    for(int i = 0; i < decimal.size(); i++) if(decimal[i]==1) str_decimal.push_back(to_string(i));
+    
+    for(string temp : str_decimal){
+        vector<int> repeat_temp(repeat_cnt);
+        int flag = 0;
+        for(int i = 0; i < temp.size(); i++){
+            repeat_temp[temp[i] - '0']--;
+        }
+        for(int j : repeat_temp) if(j<0) flag=1;
+        
+        if(flag==0) answer++;
+    }
+    
+    
     return answer;
 }
