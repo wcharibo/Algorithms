5abee55805dc3fbf33bd8fb68acca02a48f29163
wcharibo <80238487+wcharibo@users.noreply.github.com>
2024-10-17 23:09:10 +0900

    [level 3] Title: 네트워크, Time: 2.76 ms, Memory: 6.44 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.08 ms
+메모리: 6.44 MB, 시간: 2.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 17일 21:36:34
+2024년 10월 17일 23:09:08
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,36 +3,26 @@
 
 using namespace std;
 
-int answer = 0;
-
-void dfs(vector<vector<int>> &com_table, int x, int y);
-
-int solution(int n, vector<vector<int>> computers) {
-    
-    for(int i = 0; i < computers.size(); i++){
-        int flag = 0;
-        for(int j = 0; j < computers[i].size(); j++){
-            if(flag !=0 && i != j && computers[i][j] == 1){
-                dfs(computers, i, j);
-            }
-            else if(i != j && computers[i][j] == 1){
-                flag = 1;
-                answer++;
-                dfs(computers, i, j);
-            }
+void dfs(vector<vector<int>> com_table, vector<int> &visited, int i){
+    for(int k = 0; k < com_table[i].size(); k++){
+        if(com_table[i][k]==1 && visited[k] != 1 && i != k){
+            visited[k] = 1;
+            dfs(com_table, visited, k);
         }
-        if(computers[i][i] !=0 && flag ==0) answer++;
     }
-    return answer;
 }
 
-void dfs(vector<vector<int>> &com_table, int x, int y){
-    com_table[x][y] = 0;
-    com_table[y][x] = 0;
-    com_table[y][y] = 0;
-    for(int k = 0; k < com_table[y].size(); k++){
-        if(com_table[y][k]==1){
-            dfs(com_table, y, k);
-        }
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    vector<int> visited(n, 0);
+    
+    for(int i = 0; i < n; i++){
+        if(visited[i] == 1) continue;
+        
+        visited[i] = 1;
+        answer++;
+        
+        dfs(computers, visited, i);
     }
+    return answer;
 }
