b41b501bee103a805912c397b24aa7756ff93d67
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-02-13 16:27:36 +0900

    [level 2] Title: 큰 수 만들기, Time: 2638.99 ms, Memory: 5.45 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,24 +1,21 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    vector<pair<char, int>> numbs, result;
     int start = 0, target = number.size() - k;
     
-    for(int i = 0; i < number.size(); i++) numbs.push_back({number[i], 1});
-    
     for(int i = target; i > 0; i--){
-        pair<char, int> max = {0,0};
+        int max = 0;
         for(int j = start; j <= number.size() - i; j++){
-            if(numbs[j].second && number[j] > max.first) max = {number[j], j}; 
+            if(number[j] > max){
+                max = number[j];
+                start = j+1;
+            }
         }
-        answer+=max.first;
-        start = max.second+1;
-        numbs[max.second].second = -1;
+        answer+=max;
     }
     
     return answer;
