1ee16c19d849b23cc51380c0dd4a0e75653a0376
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-02-17 22:30:30 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.05 ms
+메모리: 4.19 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 17일 21:49:57
+2025년 02월 17일 22:30:28
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,14 +1,23 @@
 #include <string>
-#include <vector>
-#include <cmath>
 
 using namespace std;
 
 int solution(int storey) {
-    int answer = 0;
-    
-    for(int i = 0; i <= 8; i++){
+    int answer = 0, remain = 0;
+
+    while(storey > 0){
+        remain = storey%10;
+        storey = storey/10;
         
+        if(remain == 5 && storey != 0){
+            if(storey%10 >= 5) storey++;
+            answer+=remain;
+        }
+        else if(remain <=5) answer+=remain;
+        else{
+            answer+=(10-remain);
+            storey++;
+        }
     }
     
     return answer;
