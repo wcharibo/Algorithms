9a0e1b7b7ce980ee9f9aad987e48e72949fd7285
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-02-17 22:36:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -9,11 +9,15 @@ int solution(int storey) {
         remain = storey%10;
         storey = storey/10;
         
-        if(remain > 5 || (remain == 5 && storey%10 >= 5)){
-            storey++;
-            answer+=(10-remain);
+        if(remain == 5 && storey != 0){
+            if(storey%10 >= 5) storey++;
+            answer+=remain;
         }
-        else answer+=remain;
+        else if(remain <=5) answer+=remain;
+        else{
+            answer+=(10-remain);
+            storey++;
+        }
     }
     
     return answer;
