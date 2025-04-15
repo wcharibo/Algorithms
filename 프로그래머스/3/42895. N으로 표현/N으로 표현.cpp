ccdb87042a5fb169e575097f0a5c9028312e439a
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-04-16 03:08:05 +0900

    [level 3] Title: N으로 표현, Time: 2.09 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,36 +1,36 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(int N, int number) {
     int answer = -1, cnt = 1;
-    vector<vector<int>> arr(9);
+    vector<unordered_set<int>> arr(9);
     
-    arr[0].push_back(0);
+    arr[0].insert(0);
     for(int i = 1; i < arr.size(); i++){
-        int temp = arr[i-1][0]*10+N;
+        int temp = (*arr[i-1].begin())*10+N;
         if(temp == number) return i;
-        else arr[i].push_back(temp);
+        else arr[i].insert(temp);
     }
     
     while(cnt < 9){
         for(int i = cnt - 1; i >= cnt - i; i--){
-            for(int j = 0; j < arr[i].size(); j++){
-                for(int k = 0; k < arr[cnt - i].size(); k++){
+            for(auto j = arr[i].begin(); j != arr[i].end(); j++){
+                for(auto k = arr[cnt - i].begin(); k != arr[cnt - i].end(); k++){
                     vector<int> temp(6);
-                    temp[0] = arr[i][j] + arr[cnt - i][k];
-                    temp[1] = arr[i][j] - arr[cnt - i][k];
-                    temp[2] = arr[i][j] / arr[cnt - i][k];
-                    temp[3] = arr[i][j] * arr[cnt - i][k];
-                    temp[4] = arr[cnt - i][k] - arr[i][j];
-                    temp[5] = arr[cnt - i][k] / arr[i][j];
+                    temp[0] = *j + *k;
+                    temp[1] = *j - *k;
+                    temp[2] = *j / *k;
+                    temp[3] = *j * *k;
+                    temp[4] = *k - *j;
+                    temp[5] = *k / *j;
                     
                     for(int t : temp){
                         if(t == number) return cnt;
                         else if(t > 0 ){
-                            arr[cnt].push_back(t);
+                            arr[cnt].insert(t);
                         }
                     }
                 }
