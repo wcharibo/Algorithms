bb4b354ff2afd6253af871554f99b6d474b31809
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-04-16 03:26:17 +0900

    [level 3] Title: N으로 표현, Time: 1.91 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -18,15 +18,15 @@ int solution(int N, int number) {
     
     while(cnt < 9){
         for(int i = cnt - 1; i >= cnt - i; i--){
-            for(auto j = arr[i].begin(); j != arr[i].end(); j++){
-                for(auto k = arr[cnt - i].begin(); k != arr[cnt - i].end(); k++){
+            for(auto j : arr[i]){
+                for(auto k : arr[cnt - i]){
                     vector<int> temp(6);
-                    temp[0] = *j + *k;
-                    temp[1] = *j - *k;
-                    temp[2] = *j / *k;
-                    temp[3] = *j * *k;
-                    temp[4] = *k - *j;
-                    temp[5] = *k / *j;
+                    temp[0] = j + k;
+                    temp[1] = j - k;
+                    temp[2] = j / k;
+                    temp[3] = j * k;
+                    temp[4] = k - j;
+                    temp[5] = k / j;
                     
                     for(int t : temp){
                         if(t == number) return cnt;
