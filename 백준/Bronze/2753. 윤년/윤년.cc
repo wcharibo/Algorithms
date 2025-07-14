217bff8afd450ee834ce0974879121c81b86af89
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-14 23:24:03 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int year;
+    cin>>year;
+    
+    if((year%4 == 0 && year%100 != 0) || year%400 ==0) cout<<1;
+    else cout<<0;
+    
+}
