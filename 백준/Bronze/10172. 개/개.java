ffc23c2e355448d23e66df50aa879fefff6ac0f1
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-23 14:35:13 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|");
+        
+       System.out.println(sb);
+    }
+}
