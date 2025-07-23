ada73d45f562ddb5a27dff81aca8ba9e35187e7d
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-23 14:33:18 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+        
+       System.out.println(sb);
+    }
+}
