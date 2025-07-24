af2d52864a75575bd61fd2bac00003524375ae7d
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-24 23:43:48 +0900

    [Bronze V] Title: A×B, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+        
+        sb.append(a*b);
+        System.out.println(sb);
+    }
+}
