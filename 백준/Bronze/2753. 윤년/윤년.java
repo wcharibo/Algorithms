31abae85502dbc4caaaee2067078ddd413580ef4
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-24 23:55:43 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        
+        if((year%4 == 0 && year%100 != 0) || year%400 ==0) System.out.println(1);
+        else System.out.println(0);
+    }
+}
