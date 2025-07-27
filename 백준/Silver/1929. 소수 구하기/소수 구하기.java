e0f3d6afb4397a170b400da4a3ce487ff0ca7b0c
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-07-27 16:45:38 +0900

    [Silver III] Title: 소수 구하기, Time: 256 ms, Memory: 22272 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder result = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		boolean[] prime = new boolean[M+1];
+		Arrays.fill(prime, true);
+		int start = 2;
+		
+		while(start < M){
+		    if(prime[start]){
+    		    int multipleMax = M/start;
+    		    
+    		    for(int j = 2; j <= multipleMax; j++){
+    		        prime[start*j] = false;   
+    		    }
+		        
+		    }
+		    
+		    start++;
+		}
+		
+		if(N < 2) N = 2;
+		
+		for(int i = N; i < prime.length; i++) if(prime[i]) result.append(i).append("\n");
+		
+		System.out.println(result);
+	}
+}
