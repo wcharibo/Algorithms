a7facc063121046d06fddb4020937e654cc01271
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-08-09 23:07:52 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -9,20 +9,19 @@ class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int max = 0;
+
 		int[] bag = new int[K+1];
-		int[] weight = new int[N+1];
-		int[] value = new int[N+1];
+		int[][] obj = new int[N+1][2];
 		
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			weight[i] = Integer.parseInt(st.nextToken());
-			value[i] = Integer.parseInt(st.nextToken());
+			obj[i][0] = Integer.parseInt(st.nextToken());
+			obj[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
 		for(int i = 1; i <=N; i++) {
-			for(int j = K; j-weight[i] >=0; j--) {
-				bag[j] = Math.max(bag[j], value[i] + bag[j-weight[i]]);
+			for(int j = K; j-obj[i][0] >=0; j--) {
+				bag[j] = Math.max(bag[j], obj[i][1] + bag[j-obj[i][0]]);
 			}
 		}
 		
