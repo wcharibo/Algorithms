d965976b3bd4da8726f438142e9a687ce1e228a6
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-08-17 23:29:23 +0900

    [Gold II] Title: 빵집, Time: 364 ms, Memory: 29592 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static boolean[][] map;
+	static int[][] dir = { { -1, 1 }, { 0, 1 }, { 1, 1 } };
+
+	static boolean dfs(int curRow, int curCol) {
+		if (curCol == map[0].length -1 && curRow == map.length -1) {
+			return true;
+		} else if (curCol == map[0].length -1 ) {
+			return true;
+		}
+		
+		
+		for(int i = 0; i < 3; i++) {
+			int nextRow = curRow + dir[i][0], nextCol = curCol + dir[i][1];
+			
+			boolean flag = false;
+			
+			if(0 <= nextRow && nextRow< map.length && 0 <= nextCol && nextCol < map[0].length) {
+				if(!map[nextRow][nextCol]) {
+					map[nextRow][nextCol] = true;
+					flag = dfs(nextRow, nextCol);
+					if(flag) return flag;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		map = new boolean[R][C];
+
+		for (int i = 0; i < R; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < C; j++) {
+				if(str.charAt(j) == 'x') map[i][j] = true;
+			}
+		}
+		
+		for(int i = 0; i < R; i++) {
+			dfs(i, 0);
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i < R; i++) {
+			if(map[i][C-1]) cnt++;
+		}
+		
+		System.out.println(cnt);
+
+	}
+}
