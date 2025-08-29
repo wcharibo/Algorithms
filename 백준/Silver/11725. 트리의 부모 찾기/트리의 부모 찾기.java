004aaaf37883c648417015ca6f5aadd1147f1d82
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-08-29 11:08:04 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1052 ms, Memory: 79836 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+import java.awt.*;
+
+public class Main {
+	static ArrayList<Integer>[] edges;
+	static int[] roots;
+	
+	static void connect(int cur) {
+		for(int i = 0; i < edges[cur].size(); i++) {
+			if(roots[edges[cur].get(i)] != 0) {
+				roots[cur] = edges[cur].get(i);
+			}
+			else {
+				roots[edges[cur].get(i)] = cur;
+				connect(edges[cur].get(i));
+			}
+			
+		}
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		edges = new ArrayList[N+1];
+		roots = new int[N+1];
+		
+		roots[1] = 1;
+		
+		for(int i = 1; i <= N; i++) edges[i] = new ArrayList<>();
+
+		for (int i = 1; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			edges[a].add(b);
+			edges[b].add(a);
+		}
+		
+		connect(1);
+		
+		for(int i = 2; i < roots.length; i++) {
+			System.out.println(roots[i]);
+		}
+	}
+}
