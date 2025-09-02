4c62134574e6f6a930150086b5a7dabeca029637
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-09-02 09:39:57 +0900

    [Gold III] Title: 파티, Time: 1240 ms, Memory: 237372 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,6 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static class Pair implements Comparable<Pair>{
+		int target;
+		int fare;
+		
+		Pair(int target, int fare){
+			this.target = target;
+			this.fare = fare;
+		}
+		
+		public int compareTo(Pair latter) {
+			return this.fare - latter.fare;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,34 +26,67 @@ public class Main {
 		int X = Integer.parseInt(st.nextToken()) -1;
 
 		int[][] map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(map[i], 1000000);
+		ArrayList<Pair>[] edges = new ArrayList[N];
+		for(int i = 0; i < N; i++) {
+			edges[i] = new ArrayList<>();
 		}
-
+		int [][] dist = new int[N][N];
+		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
-			map[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = Integer
-					.parseInt(st.nextToken());
+//			map[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = Integer
+//					.parseInt(st.nextToken());
+			
+			edges[Integer.parseInt(st.nextToken()) - 1].add(new Pair(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken())));
 		}
 
-		for (int i = 0; i < N; i++) {
-			map[i][i] = 0;
-		}
+		
+		for(int start = 0; start < N; start++) {
 
-		for (int k = 0; k < N; k++) {
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-						map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+			PriorityQueue<Pair> pq = new PriorityQueue<>();
+			HashSet<Integer> vist = new HashSet<>();
+			
+			vist.add(start);
+			Arrays.fill(dist[start], 100000);
+			dist[start][start] = 0;
+			pq.add(new Pair(start, 0));
+			
+			while(!pq.isEmpty() && vist.size() != N) {
+				Pair cur = pq.poll();
+
+				vist.add(cur.target);
+				
+//				for(int i = 0; i < N; i++) {
+//					if(map[cur.target][i] != 0 && !vist.contains(i)) {
+//						int temp = cur.fare + map[cur.target][i];
+//						if(dist[start][i] > temp) {
+//							dist[start][i] = temp;
+//							pq.add(new Pair(i, temp));
+//						}
+//					}
+//				}
+				
+				for(Pair next : edges[cur.target]) {
+					if(!vist.contains(next.target)) {
+						int temp = cur.fare + next.fare;
+						if(dist[start][next.target] > temp) {
+							dist[start][next.target] = temp;
+							pq.add(new Pair(next.target, temp));
+						}
+					}
 				}
-			}
+				
+				
+			}	
 		}
-
+		
+		
 		int[] result = new int[N];
 
 		for (int i = 0; i < N; i++) {
-			result[i] += map[i][X];
-			result[i] += map[X][i];
+			result[i] += dist[i][X];
+			result[i] += dist[X][i];
 		}
 
 		int max = Integer.MIN_VALUE;
