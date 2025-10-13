430116ea3818d2622dd20a9508d70c6714d7889c
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-10-14 01:15:54 +0900

    [Gold III] Title: 감시, Time: 264 ms, Memory: 37836 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -43,52 +43,61 @@ public class Main {
 
 	private static void find(int[] directions, int cur) {
 		if (cur == directions.length) {
-			fill(-1, directions);
 			result = Math.min(result, calc());
-			fill(0, directions);
 			return;
 		}
-		
+
 		int cx = CCTVs.get(cur)[0];
 		int cy = CCTVs.get(cur)[1];
 		for (int i = 0; i < directs[office[cx][cy]].length; i++) {
 			directions[cur] = i;
+			List<int[]> changed = fill(cur, i);
 			find(directions, cur + 1);
+			revert(changed);
 		}
+
 	}
 
-	private static void fill(int val, int[] directions) {
-		for (int cur = 0; cur < directions.length; cur++) {
-			int dir = directions[cur];
-			int cx = CCTVs.get(cur)[0];
-			int cy = CCTVs.get(cur)[1];
-			List<Integer> dirList = new ArrayList<>();
-			dirList.add(dir);
+	private static void revert(List<int[]> changed) {
+		for(int[] point : changed) {
+			office[point[0]][point[1]] = 0;
+		}
+		changed.clear();
+	}
 
-			for (int j : directs[office[cx][cy]][directions[cur]]) {
-				dirList.add(j);
-			}
+	private static List<int[]> fill(int cur, int dir) {
+		int cx = CCTVs.get(cur)[0];
+		int cy = CCTVs.get(cur)[1];
+		List<Integer> dirList = new ArrayList<>();
+		List<int[]> changed = new ArrayList<>();
+		dirList.add(dir);
 
-			for (int d : dirList) {
-				for (int i = 1; i < 8; i++) {
-					int nx = cx + (i * dirs[d][0]);
-					int ny = cy + (i * dirs[d][1]);
+		for (int j : directs[office[cx][cy]][dir]) {
+			dirList.add(j);
+		}
 
-					if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-						if (office[nx][ny] == 6)
-							break;
+		for (int d : dirList) {
+			for (int i = 1; i < 8; i++) {
+				int nx = cx + (i * dirs[d][0]);
+				int ny = cy + (i * dirs[d][1]);
 
-						if (office[nx][ny] > 0)
-							continue;
-
-						office[nx][ny] = val;
-
-					} else
+				if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+					if (office[nx][ny] == 6)
 						break;
-				}
+
+					if (office[nx][ny] > 0 || office[nx][ny] == -1)
+						continue;
+					
+					office[nx][ny] = -1;
+					changed.add(new int[] {nx, ny});
+				} else
+					break;
 			}
 		}
+		
+		return changed;
 	}
+
 
 	private static int calc() {
 		int cnt = 0;
