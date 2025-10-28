c66351da01369390bee09f404bd2d84a9b0ca97e
wcharibo <80238487+wcharibo@users.noreply.github.com>
2025-10-28 14:56:24 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 396 ms, Memory: 40688 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,71 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int[][] forest, dist;
-	static int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-	
-	public static int dfs(int i , int j) {
-		int cur = forest[i][j];
-		int candidates[] = new int[4];
-		
-		if(dist[i][j] != 0) return dist[i][j];
-		
-		for(int dir = 0; dir < 4; dir++) {
-			int ni = i + dirs[dir][0];
-			int nj = j + dirs[dir][1];
-//			System.out.println(i + "    " + j);
-			if(0 <= ni && ni < n && 0 <= nj && nj < n && forest[ni][nj] > cur) {
-				if(dist[ni][nj] != 0) {
-					candidates[dir] = dist[ni][nj] + 1;
-					continue;
-				}
-				
-				candidates[dir] = dfs(ni, nj);
-			}			
-		}
-		
-		int max = Integer.MIN_VALUE;
-		for(int cnt = 0; cnt < candidates.length; cnt++) {
-			max = Math.max(max, candidates[cnt]);
-		}
-		
-		dist[i][j] = max;
-		
-		return dist[i][j]+1;
-	}
-	
-	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int[][] forest, dist;
+    static int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-		n = Integer.parseInt(br.readLine());
+    public static int dfs(int i, int j) {
+        if (dist[i][j] != 0) return dist[i][j];
 
-		forest = new int[n][n];
+        dist[i][j] = 1; // 자기 자신 포함
+        int cur = forest[i][j];
 
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				forest[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        for (int[] d : dirs) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+            if (0 <= ni && ni < n && 0 <= nj && nj < n && forest[ni][nj] > cur) {
+                dist[i][j] = Math.max(dist[i][j], 1 + dfs(ni, nj));
+            }
+        }
+        return dist[i][j];
+    }
 
-		dist = new int[n][n];
-		
-		int result = Integer.MIN_VALUE;
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				dfs(i, j);
-				
-				result = Math.max(result, dist[i][j]);
-			}
-		}
-		
-		System.out.println(result+1);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        forest = new int[n][n];
+        dist = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                forest[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result = Math.max(result, dfs(i, j));
+            }
+        }
+
+        System.out.println(result);
+    }
 }
