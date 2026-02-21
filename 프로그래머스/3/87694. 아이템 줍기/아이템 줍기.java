f1850a284290d3f3f262ee2ce0bd1e5243e2d1f3
wcharibo <80238487+wcharibo@users.noreply.github.com>
2026-02-21 17:57:33 +0900

    [level 3] Title: 아이템 줍기, Time: 1.36 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -24,9 +24,11 @@ class Solution {
     }
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        
+        // 중간 위치도 포함되는지 파악하기 위해 2배 해주기
         for(int i = 0 ; i < rectangle.length; i++){
             for(int j = 0 ; j < 4; j++){
-                rectangle[i][j]*=2;
+                rectangle[i][j] *= 2;
             }
         }
         
@@ -36,6 +38,7 @@ class Solution {
         itemY *= 2;
         
         int answer = 0;
+        // 이동도 2배로
         int[][] dirs = {{-2, 0},{2, 0},{0, -2},{0, 2}};
         
         Queue<Human> q = new ArrayDeque<>();
@@ -52,15 +55,19 @@ class Solution {
             for(int dir = 0 ; dir < 4; dir++){
                 Pair cur = new Pair(h.cur.x, h.cur.y);
                 Pair log = new Pair(cur.x, cur.y);
+                //이동이 직사각형 선분 위에서 이뤄졌는가
                 boolean isOnRec = false;
+                //
                 boolean isPos = true;
                 
+                //도착위치
                 cur.x += dirs[dir][0];
                 cur.y += dirs[dir][1];
+                //중간위치 파악
                 log.x += (dirs[dir][0]/2);
                 log.y += (dirs[dir][1]/2);
                 
-                
+                //이전 위치인지 확인
                 if(cur.x == h.prev.x && cur.y == h.prev.y) continue;
                 
                 //{x1, y1, x2, y2}
@@ -68,15 +75,15 @@ class Solution {
                     //밖에 있는 경우
                     if(cur.x < rec[0] || cur.x > rec[2] || cur.y < rec[1] || cur.y > rec[3]) continue;
                     
-                    //안에 있는 경우
+                    //안에 있는 경우(이동 과정 위의 위치도 검사)
                     if((cur.x > rec[0] && cur.x < rec[2] && cur.y > rec[1] && cur.y < rec[3]) || 
                       (log.x > rec[0] && log.x < rec[2] && log.y > rec[1] && log.y < rec[3])){
                         isPos = false;
                         continue;
                     }
                     
+                    //이동과정이 사각형의 선분 위였는 지 확인
                     if(rec[0] <= log.x && log.x <= rec[2] && rec[1] <= log.y && log.y <= rec[3]){
-                        
                         isOnRec = true;
                     }
                 }
@@ -87,7 +94,6 @@ class Solution {
             }
             
         }
-        
         
         return answer;
     }
