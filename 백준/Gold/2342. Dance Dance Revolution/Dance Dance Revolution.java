03c8779f4c4b8d6fd0541b802656475e4a88def7
wcharibo <80238487+wcharibo@users.noreply.github.com>
2026-02-25 01:04:20 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 680 ms, Memory: 104460 KB -BaekjoonHub

---
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    public static class Pair{
+        int x;
+        int y;
+        int power;
+
+        Pair(int x, int y, int power){
+            this.x = Math.min(x, y);
+            this.y = Math.max(x, y);
+            this.power = power;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            Pair p = (Pair) o;
+            return this.x == p.x && this.y == p.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.x, this.y);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        int[][] cost = {{0,2,2,2,2},{0,1,3,4,3},{0,3,1,3,4},{0,4,3,1,3},{0,3,4,3,1}};
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int next = Integer.parseInt(st.nextToken());
+        int answer = Integer.MAX_VALUE;
+
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(0, 0, 0));
+
+        while(true){
+
+            int[][] powers = new int[5][5];
+
+            for(int i = 0; i < 5; i++){
+                Arrays.fill(powers[i], Integer.MAX_VALUE);
+            }
+
+            Set<Pair> cand = new HashSet<>();
+
+            while(!q.isEmpty()){
+                Pair cur = q.poll();
+
+                if(next != cur.y && powers[next][cur.y] > cur.power + cost[cur.x][next]){
+                    powers[next][cur.y] = cur.power + cost[cur.x][next];
+                }
+
+                if(next != cur.x && powers[cur.x][next] > cur.power + cost[cur.y][next]){
+                    powers[cur.x][next] = cur.power + cost[cur.y][next];
+                }
+            }
+
+            for(int i = 0; i < powers.length; i++){
+                for(int j = 0; j < powers[i].length; j++){
+                    if(powers[i][j] != Integer.MAX_VALUE){
+                        cand.add(new Pair(i, j, Math.min(powers[i][j], powers[j][i])));
+                    }
+                }
+            }
+
+            for(Pair i : cand){
+                q.add(i);
+            }      
+
+            next = Integer.parseInt(st.nextToken());
+            if(next == 0){
+
+                for(int i[] : powers){
+                    for(int j : i){
+                        answer = Math.min(answer, j);
+                    }
+                }
+
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
