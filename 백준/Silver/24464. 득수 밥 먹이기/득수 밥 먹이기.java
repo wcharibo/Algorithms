7fc9fad37a5a080d40d6c10a911bcd98e5e512d1
wcharibo <80238487+wcharibo@users.noreply.github.com>
2026-02-26 22:22:54 +0900

    [Silver I] Title: 득수 밥 먹이기, Time: 96 ms, Memory: 22612 KB -BaekjoonHub

--- 백준/Silver/24464. 득수 밥 먹이기/득수 밥 먹이기.java
+++ 백준/Silver/24464. 득수 밥 먹이기/득수 밥 먹이기.java
@@ -12,10 +12,10 @@ class Main{
         final long Big = 1000000007;
         
         long[] lunch = {1,1,1,1,1}; 
-        
+        long[] prev;
         
         for(int i = 1; i < N; i++) {
-        	long[] prev = lunch.clone();
+        	prev = lunch.clone();
         	
         	lunch[0] = (prev[1] + prev[2] + prev[3] + prev[4])%Big;
         	lunch[1] = (prev[0] + prev[3] + prev[4])%Big;
@@ -26,11 +26,10 @@ class Main{
         
         for(int i = 0; i < lunch.length; i++) {
         	answer += lunch[i];
-        	answer %= Big;
         }
         
         
-        System.out.println(answer);
+        System.out.println(answer%Big);
     }
 
 }
